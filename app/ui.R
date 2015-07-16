5c172af27c056a46e20cab98776f49870da62ea4
Christian Theil Have <christiantheilhave@gmail.com>
2015-07-16 15:16:06 +0200

    Shiny/picat app to match between extremes of a distribution

---
+++ app/ui.R
@@ -0,0 +1,32 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Extreme match"),
+
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bmi",
+                  "Maximal BMI difference:",
+                  min = 0,
+                  max = 2,
+                  value = 1,
+				  step = 0.01),
+
+      sliderInput("samples",
+                  "Samples per group:",
+                  min = 1,
+                  max = 20,
+                  value = 5)
+
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
